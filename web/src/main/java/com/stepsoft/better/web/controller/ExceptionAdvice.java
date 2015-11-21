f1855e29e2b29a5ed8252ed8ab246494e936cd98
Eugene Stepanenkov <step1@bk.ru>
2015-11-21 10:27:40 +0200

    data as separate module

---
+++ web/src/main/java/com/stepsoft/better/web/controller/ExceptionAdvice.java
@@ -0,0 +1,12 @@
+package com.stepsoft.better.web.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author Eugene Stepanenkov
+ */
+@ControllerAdvice(assignableTypes = CrudController.class)
+public class ExceptionAdvice {
+
+
+}
